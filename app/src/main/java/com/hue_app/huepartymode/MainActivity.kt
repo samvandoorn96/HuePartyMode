fba2bf1f1837f8afe2d6ac8f0f845213abe86b3f
samvandoorn96 <43239312+samvandoorn96@users.noreply.github.com>
2021-11-03 12:42:42 +0100

    Init

---
+++ app/src/main/java/com/hue_app/huepartymode/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hue_app.huepartymode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
